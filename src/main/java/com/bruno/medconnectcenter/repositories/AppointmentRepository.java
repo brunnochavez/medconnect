efa9f96ccc928c9a554bbaa68e3d3650fd5307d8
Bruno Oliveiram <bruno.o.s@live.com>
2026-07-04 16:53:51 -0300

    criacao das classes AppointmentRepository e AppointmentService

--- src/main/java/com/bruno/medconnectcenter/repositories/AppointmentRepository.java
+++ src/main/java/com/bruno/medconnectcenter/repositories/AppointmentRepository.java
@@ -1,6 +1,21 @@
 package com.bruno.medconnectcenter.repositories;
 import com.bruno.medconnectcenter.entities.Appointment;
+import com.bruno.medconnectcenter.entities.AppointmentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+    Boolean existsByDoctorIdAndAppointmentDateTimeAndStatus(
+            Long doctorId,
+            LocalDateTime dateTime,
+            AppointmentStatus status
+    );
+
+    Boolean existsByPatientIdAndAppointmentDateTimeAndStatus(
+            Long patientId,
+            LocalDateTime dateTime,
+            AppointmentStatus status
+    );
 }
---
+++ src/main/java/com/bruno/medconnectcenter/services/AppointmentService.java
@@ -0,0 +1,71 @@
+package com.bruno.medconnectcenter.services;
+import com.bruno.medconnectcenter.dtos.AppointmentRequestDTO;
+import com.bruno.medconnectcenter.dtos.AppointmentResponseDTO;
+import com.bruno.medconnectcenter.entities.Appointment;
+import com.bruno.medconnectcenter.entities.AppointmentStatus;
+import com.bruno.medconnectcenter.repositories.AppointmentRepository;
+import com.bruno.medconnectcenter.repositories.DoctorRepository;
+import com.bruno.medconnectcenter.repositories.PatientRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class AppointmentService {
+
+    private final AppointmentRepository appointmentRepository;
+    private final PatientRepository patientRepository;
+    private final DoctorRepository doctorRepository;
+
+    @Transactional
+    public AppointmentResponseDTO insert(AppointmentRequestDTO dto){
+
+        boolean doctorConflict = appointmentRepository.existsByDoctorIdAndAppointmentDateTimeAndStatus(
+          dto.doctorId(),
+          dto.appointmentDateTime(),
+          AppointmentStatus.AGENDADA
+        );
+
+        if(doctorConflict){
+            throw new IllegalArgumentException("Médico já possui um agendamento para o horário informado!");
+        }
+
+        boolean patientConflict = appointmentRepository.existsByPatientIdAndAppointmentDateTimeAndStatus(
+                dto.patientId(),
+                dto.appointmentDateTime(),
+                AppointmentStatus.AGENDADA
+        );
+
+        if(patientConflict){
+            throw new IllegalArgumentException("O paciente já possui um agendamento para o horário informado!");
+        }
+
+        if(dto.appointmentDateTime().isBefore(LocalDateTime.now())){
+            throw new IllegalArgumentException("A data da consulta não pode estar no passado!");
+        }
+
+        Appointment appointment = new Appointment();
+        appointment.setAppointmentDateTime(dto.appointmentDateTime());
+        appointment.setObservations(dto.observations());
+        appointment.setStatus(AppointmentStatus.AGENDADA);
+        appointment.setPatient(patientRepository.getReferenceById(dto.patientId()));
+        appointment.setDoctor(doctorRepository.getReferenceById(dto.doctorId()));
+
+        appointment = appointmentRepository.save(appointment);
+
+        return toResponseDto(appointment);
+    }
+
+    private AppointmentResponseDTO toResponseDto(Appointment appointment) {
+        return new AppointmentResponseDTO(
+                appointment.getId(),
+                appointment.getAppointmentDateTime(),
+                appointment.getObservations(),
+                appointment.getStatus(),
+                appointment.getDoctor().getId(),
+                appointment.getPatient().getId()
+        );
+    }
+}
